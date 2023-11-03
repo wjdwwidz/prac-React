9888e9f7fc00184c684e674bbdeba28cc53424e6
wjdwwidz <wjdwwidz@gmail.com>
2023-11-03 14:51:31 +0900

    props data 렌더링

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,24 @@
-import React ,{Component} from 'react';
+import React, {Component} from 'react';
 import TOC from "./Components/TOC"
 import Content from "./Components/Content"
 import Subject from "./Components/Subject"
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
   render(){
     return (
       <div className="App">
         React
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
