5a187bc41aa9e36c95b65aab2b1788609edf68b7
wjdwwidz <wjdwwidz@gmail.com>
2023-11-08 15:08:33 +0900

    component event

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode:"read",
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome',desc:'Hello, React!!'},
       contents:[
@@ -25,30 +26,38 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i += 1;
+      }
+      // _title = this.state.contents[0].title;
+      // _desc = this.state.contents[0].desc;
     }
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}
           onChangePage = {function(){
-            this.setState({mode:'welcome'})
-          }.bind(this)}>
-        </Subject> */}
-        <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            // this.state.mode ='welcome';
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+          >
+        </Subject>
+        <TOC 
+          onChangePage={function(id){
             this.setState({
-              mode:'welcome'
+              mode:'read',
+              selected_content_id:Number(id)
             });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
-        <TOC data={this.state.contents}></TOC>
+          }.bind(this)}
+          data={this.state.contents}
+          ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     )
--- react-app/src/Components/TOC.js
+++ react-app/src/Components/TOC.js
@@ -2,11 +2,24 @@ import React, {Component} from 'react';
 
 class TOC extends Component {
     render() {
+      console.log('TOC render');
       var lists = [];
       var data = this.props.data;
       var i = 0;
       while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+        lists.push(<li key={data[i].id}>
+          <a 
+          href={"/content/"+data[i].id}
+          //if) data-something={data[i].id}
+          data-id= {data[i].id}
+          onClick={function(e){
+            e.preventDefault();
+            //console.log(e.target.dataset.id); 몇 번 째 content인지 확인
+            this.props.onChangePage(e.target.dataset.id);
+            //this.props.onChangePage(e.target.dataset.something);
+          }.bind(this)}
+          >{data[i].title}</a>
+          </li>);
         i += 1;
       }
       return (
