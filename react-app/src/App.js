939d15a4bffc2568fbf107dce1342d08ef742118
wjdwwidz <wjdwwidz@gmail.com>
2023-11-08 18:39:03 +0900

    create 구현:onsubmit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -41,7 +41,11 @@ class App extends Component {
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if (this.state.mode === 'create'){
-      _article = <CreateContent></CreateContent>
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        //add content to this.state.content
+        //setState()
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
     return (
       <div className="App">
