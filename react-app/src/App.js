4a5d56eaf6fc0c767781c0d6b759b4cf01a017a9
wjdwwidz <wjdwwidz@gmail.com>
2023-11-08 19:39:42 +0900

    create 구현:contents변경

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 class App extends Component {
   constructor (props){
     super(props);
+    this.max_content_id = 3; //마지막 content값
     this.state = {
       mode:"read",
       selected_content_id:3,
@@ -43,6 +44,18 @@ class App extends Component {
     }else if (this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         //add content to this.state.content
+        this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //   {id: this.max_content_id, title:_title, desc:_desc}
+        // );
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          // contents : this.state.contents
+          contents : _contents
+        });
+        
         //setState()
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
